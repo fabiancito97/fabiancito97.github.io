f1496b6949e5d684c59b4faa91bdffa23ba5abdc
Fabian <fabian.e.gonzalez97@gmail.com>
2022-08-17 23:35:48 -0300

    lpm

--- home/home-controller.js
+++ home/home-controller.js
@@ -1,6 +1,7 @@
 angular.module('pvtApp').controller('HomeCtrl', function ($scope, $location) {
 	$scope.buildLink = function(){
-      return $location.path()
+      return $location.absUrl();
+      
     }
 
 });
