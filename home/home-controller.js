b2e285ac314943934089bdba9c3f07d02019ae0d
Fabian <fabian.e.gonzalez97@gmail.com>
2022-08-17 22:35:16 -0300

    upload

--- home/home-controller.js
+++ home/home-controller.js
@@ -4,6 +4,8 @@ angular.module('pvtApp').controller('HomeCtrl', function ($scope) {
 
 // id of subject
 var url = window.location.pathname;
-var id = url.substring(url.lastIndexOf('?id=') + 4, url.lastIndexOf('#/'));
+//var id = url.substring(url.lastIndexOf('?id=') + 4, url.lastIndexOf('#/'));
+
+var id = url.substring(url.lastIndexOf('/') + 1);
 
 document.getElementById("id-pvt").innerHTML = id;
