efc44a516d3dc8327d749585e8bfbd4127e93bf1
Fabian <fabian.e.gonzalez97@gmail.com>
2022-08-17 22:46:08 -0300

    try 3

--- home/home-controller.js
+++ home/home-controller.js
@@ -4,8 +4,11 @@ angular.module('pvtApp').controller('HomeCtrl', function ($scope) {
 
 // id of subject
 var url = window.location.pathname;
+document.getElementById("id-pvt").innerHTML = url;
 //var id = url.substring(url.lastIndexOf('?id=') + 4, url.lastIndexOf('#/'));
 
-var id = url.substring(url.lastIndexOf('/') + 1);
+//var id = url.substring(url.lastIndexOf('/') + 1);
 
-document.getElementById("id-pvt").innerHTML = id;
+
+
+
