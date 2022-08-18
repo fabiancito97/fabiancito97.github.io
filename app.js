6615218870a2fa7b4275fdd9f03050ee23d11a4c
Fabian Enrique Gonzalez <79730442+fabiancito97@users.noreply.github.com>
2022-08-17 21:54:24 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-var url_main = "https://github.com/fabiancito97/fabiancito97.github.io/tree/master"
-
 var pvtApp = angular.module("pvtApp", [
     'mgcrea.ngStrap',
     'ui.router',
@@ -13,55 +11,54 @@ pvtApp.config(function($compileProvider) {
 
 pvtApp.config(function ($stateProvider) {
     $stateProvider.state('pretrial', {
-        url: url_main + '/pretrial',
+        url: '/pretrial',
         template: "<div ui-view></div>",
         controller: 'PreTrialCtrl'
     })
     .state('pretrial.instructions', {
-        url: url_main + '/instructions',
-        templateUrl: url_main + '/' + 'pretrial/pretrial.instructions.html',
+        url: '/instructions',
+        templateUrl: 'pretrial/pretrial.instructions.html',
         controller: 'PreTrialInstructionsCtrl'
     })
     .state('pretrial.countdown', {
-        url: url_main + '/countdown',
-        templateUrl: url_main + '/' + 'pretrial/pretrial.countdown.html',
+        url: '/countdown',
+        templateUrl: 'pretrial/pretrial.countdown.html',
         controller: 'PreTrialCountdownCtrl'
     })
     .state('settings', {
-        url: url_main + '/settings',
-        templateUrl: url_main + '/' + 'settings/settings.html',
+        url: '/settings',
+        templateUrl: 'settings/settings.html',
         controller: 'SettingsCtrl'
     })
     .state('trial', {
-        url: url_main + '/trial',
-        templateUrl: url_main + '/' + 'trial/trial.html',
+        url: '/trial',
+        templateUrl: 'trial/trial.html',
         controller: 'TrialCtrl'
     })
     .state('results', {
         url: '/results',
-        templateUrl: url_main + '/' + 'results/results.html',
+        templateUrl: 'results/results.html',
         controller: 'ResultsCtrl'
     })
     .state('results.trial', {
-        url: url_main + '/:trialId',
-        templateUrl: url_main + '/' + 'results/results.trial.html',
+        url: '/:trialId',
+        templateUrl: 'results/results.trial.html',
         controller: 'ResultsTrialCtrl'
     })
     .state('results.all', {
-        templateUrl: url_main + '/' + 'results/results.all.html',
+        templateUrl: 'results/results.all.html',
         controller: 'AllResultsCtrl'
     })
     .state('results.empty', {
-        templateUrl: url_main + '/' + 'results/results.empty.html'
+        templateUrl: 'results/results.empty.html'
     })
     .state('home', {
-         url: url_main + '/',
-         templateUrl: url_main + '/' + 'home/home.html',
-         controller: 'HomeCtrl'
+        url: '/',
+        templateUrl: 'home/home.html',
+        controller: 'HomeCtrl'
     })
 });
 
 pvtApp.config(function ($urlRouterProvider) {
     $urlRouterProvider.otherwise('/')
 });
-
