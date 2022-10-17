34398346c7b40e81073d15cda1f2f667d047c55e
Fabian <fabian.e.gonzalez97@gmail.com>
2022-10-17 16:20:00 -0300

    new try to host angularJS

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
-const express = require('express');
-const app = express();
-app.use(requireHTTPS);
+var express = require('express');
+var path = require('path');
+var app = express();
 
-app.use(express.static('./dist/PersonalPVT'));
-
-app.get('/*', function(req, res) {
-  res.sendFile('index.html', {root: 'dist/PersonalPVT/'}
-);
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname)); // set static files location, in this case the route, add a file name if not
+app.listen(process.env.PORT || 3000);
