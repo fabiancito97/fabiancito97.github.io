9a967b3ce1c316a254c917b3b2ffd9da07149283
Fabian <fabian.e.gonzalez97@gmail.com>
2022-10-30 20:09:34 -0300

    install postgresssql 2

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', function (req, res) {
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
   }
